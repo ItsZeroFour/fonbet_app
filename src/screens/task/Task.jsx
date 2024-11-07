241cd1ecacae39ff776d2a2ffeb90f09951f1672
ItsZeroFour <itsZeroFourX@gmail.com>
2024-11-07 14:33:08 +0300

    fix

--- src/screens/task/Task.jsx
+++ src/screens/task/Task.jsx
@@ -2,20 +2,30 @@ import React, { useEffect, useState } from "react";
 import style from "./style.module.scss";
 import Header from "../../components/header/Header";
 import taskImg from "../../assets/images/task.png";
-import { Link, useSearchParams } from "react-router-dom";
+import { Link, useLocation, useSearchParams } from "react-router-dom";
 import footballers from "../../data/footballers.json";
 
 const Task = ({ giftLink }) => {
   const [searchParams] = useSearchParams();
   const [index, setIndex] = useState(0);
+  const location = useLocation();
+  const [currentChapter, setCurrentChapter] = useState(1);
+
+  useEffect(() => {
+    if (location.state.currentChapter) {
+      setCurrentChapter(location.state.currentChapter);
+    }
+  }, [location]);
 
   useEffect(() => {
     if (searchParams.get("index") && +searchParams.get("index")) {
       setIndex(+searchParams.get("index"));
+    } else if (location.state.index) {
+      setIndex(location.state.index);
     } else {
       setIndex(0);
     }
-  }, [searchParams]);
+  }, [searchParams, location]);
 
   return (
     <div className={style.task}>
@@ -23,21 +33,47 @@ const Task = ({ giftLink }) => {
         <Header giftLink={giftLink} />
 
         <div className={style.task__text}>
-          <h3>Глава 1</h3>
-          <p>Выпускной экзамен в школе скаутов</p>
+          <h3>Глава {currentChapter}</h3>
+          <p>
+            {currentChapter === 1
+              ? "Выпускной экзамен в школе скаутов"
+              : currentChapter === 2
+              ? "Практика в середнячке РПЛ"
+              : currentChapter === 3
+              ? "Переезд в крепкий европейский клуб"
+              : "Руководство трансферами в европейском ТОП-клубе"}
+          </p>
         </div>
 
         <img className={style.task__image} src={taskImg} alt="task" />
 
         <div className={style.task__task}>
-          <h2>{footballers.items[index].round}:</h2>
+          <h2>Раунд 1:</h2>
           <p>{footballers.items[index].task}</p>
           <p>
-            Наберите <span>10 очков</span>, чтобы пройти в следующий раунд
+            Наберите{" "}
+            <span>
+              {currentChapter === 1
+                ? 6
+                : currentChapter === 2
+                ? 8
+                : currentChapter === 3
+                ? 10
+                : 12}{" "}
+              очков
+            </span>
+            , чтобы пройти в следующий раунд
           </p>
         </div>
 
-        <Link className={style.task__link} to={`/game?index=${index}`}>
+        <Link
+          className={style.task__link}
+          to={
+            currentChapter === 1
+              ? `/game?index=${index}`
+              : `/game?index=${index + 1}`
+          }
+        >
           Начать
         </Link>
       </div>
