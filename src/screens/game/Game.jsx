10d02101a0061cc0d2581a7aa48a0cd40cad5bcb
itsZeroFour <itsZeroFour@gmail.com>
2024-11-14 20:29:56 +0300

    fix

--- src/screens/game/Game.jsx
+++ src/screens/game/Game.jsx
@@ -524,7 +524,6 @@ const Game = React.memo(({ giftLink, registerLink }) => {
                             document.querySelector(
                               `.${style.swipe}`
                             ).style.transform = `translateX(0px) rotate(0deg)`;
-                            setDragX(0);
                           }
                         }}
                         style={{
